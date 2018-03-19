52a034601ca69f58c0c608c13ac07abce49202a2
Lucas Alves <lucas.arnaldo@gmail.com>
2018-03-19 15:58:13 +0100

    Interface Graphique

---
+++ Compass/app/src/main/java/com/example/timotemalherbe/compass/EnregistrementParcours.java
@@ -0,0 +1,13 @@
+package com.example.timotemalherbe.compass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnregistrementParcours extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enregistrement_parcours);
+    }
+}
--- Compass/app/src/main/java/com/example/timotemalherbe/compass/MainActivity.java
+++ Compass/app/src/main/java/com/example/timotemalherbe/compass/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.timotemalherbe.compass;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.view.View;
 import android.widget.TextView;
 
 import java.io.BufferedOutputStream;
@@ -19,24 +21,24 @@ import android.os.Environment;
 import android.util.Log;
 
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity {
 
-    private float currentDegree;
+   // private float currentDegree;
 
     // device sensor manager
-    private SensorManager mSensorManager;
+    //private SensorManager mSensorManager;
+    //TextView tvHeading;
 
-    TextView tvHeading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        currentDegree=mSensorManager.getDefaultSensor()
-
-        tvHeading = (TextView) findViewById(R.id.textView);
-
+        //currentDegree=mSensorManager.getDefaultSensor();
+        }
+        //tvHeading = (TextView) findViewById(R.id.textView);
+/*
         // initialize your android device sensor capabilities
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
@@ -104,19 +106,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 catch (IOException e) {
                     Log.e("Exception", "File write failed: " + e.toString());
 
-                }*/
+                }
 
                 h.postDelayed(this, 1000);
             }
         }, 1000);
-    }
-
+    }*/
+    /*
     @Override
     protected void onResume() {
         super.onResume();
 
         // for the system's orientation sensor registered listeners
-        mSensorManager.registerListener(this, mSensorManager.getOrientation(), SensorManager.SENSOR_DELAY_GAME);
+        //mSensorManager.registerListener(this, mSensorManager.getOrientation(), SensorManager.SENSOR_DELAY_GAME);
     }
 
     protected void onPause() {
@@ -138,6 +140,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // not in use
-    }
+    }*/
 
+    public void EnregistrerParcours(View view) {
+        Intent EnregistrerParcours = new Intent(this, EnregistrementParcours.class);
+        startActivity(EnregistrerParcours);
+    }
 }
