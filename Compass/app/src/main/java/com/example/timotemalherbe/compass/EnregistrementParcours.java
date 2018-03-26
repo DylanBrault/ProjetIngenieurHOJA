b69c7d03561373464e91a15184ab0b87161bfb29
Unknown <malherbe.timote@sfr.fr>
2018-03-26 15:04:50 +0200

    Projet réparé 2

--- Compass/app/src/main/java/com/example/timotemalherbe/compass/EnregistrementParcours.java
+++ Compass/app/src/main/java/com/example/timotemalherbe/compass/EnregistrementParcours.java
@@ -1,13 +1,81 @@
 package com.example.timotemalherbe.compass;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class EnregistrementParcours extends AppCompatActivity {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class EnregistrementParcours extends AppCompatActivity implements SensorEventListener {
+    //private float currentDegree = 0f;
+
+    // device sensor manager
+    private SensorManager mSensorManager;
+
+    TextView tvHeading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enregistrement_parcours);
+
+        //tvHeading = (TextView) findViewById(R.id.textView);
+
+        // initialize your android device sensor capabilities
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        writeToFile();
     }
+
+    public void writeToFile(){
+        try {
+            // catches IOException below
+            final String TESTSTRING = new String("Hello Android \n");
+            FileOutputStream fOut = openFileOutput("test_file.txt",
+                    MODE_APPEND);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+            // Write the string to the file
+            osw.write(TESTSTRING);
+            osw.flush();
+            osw.close();
+        } catch (IOException ioe)
+        {ioe.printStackTrace();}
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // for the system's orientation sensor registered listeners
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    protected void onPause() {
+        super.onPause();
+
+        // to stop the listener and save battery
+        mSensorManager.unregisterListener(this);
+    }
+
+
+    public void onSensorChanged(SensorEvent event) {
+
+        // get the angle around the z-axis rotated
+        final float degree = Math.round(event.values[0]);
+
+        tvHeading.setText(Float.toString(degree));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // not in use
+    }
+
+
 }
